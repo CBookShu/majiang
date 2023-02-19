e6569bdd0f4e2209b2ee04807dcce0764bfff378
cbookshu <cbookshu@outlook.com>
2023-02-19 18:30:57 +0800

    提交基础代码

---
+++ src/utils.h
@@ -0,0 +1,33 @@
+#ifndef __MAJIANG_UTILS_HEADER__
+#define __MAJIANG_UTILS_HEADER__
+
+/*
+    108张牌的组合罗列:
+    id: 0~35 万:
+    0~8     1万~9万
+    9~17    1万~9万
+    18~26    1万~9万
+    27~35    1万~9万
+    id: 36~71 条:
+    36~44   1条~9条
+    ...
+    id: 72~107 筒:
+    72~81     1筒~9筒
+    ...
+*/
+
+#define TOAL_CARDS                  108
+#define HAND_CARDS_COUNT            14
+
+#define INVALID_CARD_ID             -1
+#define SHAPE_WAN                   0
+#define SHAPE_TIAO                  1
+#define SHAPE_TONG                  2
+
+
+
+
+
+
+
+#endif  __MAJIANG_UTILS_HEADER__
